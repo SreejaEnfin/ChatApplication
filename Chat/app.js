12c9d8e16e2ad1c93aaeb6a3621e1627c4e2e142
SreejaEnfin <sreeja.pm@enfintechnologies.com>
2023-01-03 16:36:04 +0530

    update

--- Chat/app.js
+++ Chat/app.js
@@ -12,36 +12,16 @@ app.get('/', (req, res) => {
 // for connecting
 io.on('connection', (socket) => {
   console.log('a user connected');
-});
-
-// for disconnecting
-io.on('connection', (socket) => {
-  console.log('a user connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-});
-
-// for display chat
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-  });
-});
-
-
-// to emit
-io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
-
-io.on('connection', (socket) => {
   socket.broadcast.emit('hi');
-});
-
-io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
+  
 });
+
 
 server.listen(8080, () => {
   console.log('listening on *:8080');
